51c712ba39741a85e021ee54996b247eab94a013
zzeis <moisesmonteiro.stark@gmail.com>
2023-02-03 10:26:24 -0300

    change days for "days + 1"

--- js/cronometro.js
+++ js/cronometro.js
@@ -1,23 +1,30 @@
-
 var year = new Date();
 var year = year.getFullYear();
-// Data alvo
-var targetDate = new Date(year+'-03-09');
 
-// Atualiza a cada 1 segundo
-setInterval(function() {
+var targetDate = new Date(year + "-03-09");
+
+
+/* A function that is called every 1000 milliseconds. */
+setInterval(function () {
   var now = new Date();
 
   // Calcular a diferença em milissegundos
   var timeDiff = targetDate - now;
 
   // Calcular o tempo restante em dias, horas e segundos
-  var days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  var days = Math.floor(timeDiff / (1000 * 60 * 60 * 24) + 1);
   var hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 
-  // Exibir o tempo restante
-  document.querySelector("#stopwatch").innerHTML = days + " dias, " + hours + " horas, " + minutes + " minutos e " + seconds + " segundos restantes.";
-  console.log(days + " dias, " + hours + " horas, " + minutes + " minutos e " + seconds + " segundos restantes.");
+  
+  document.querySelector("#stopwatch").innerHTML =
+    days +
+    " dias, " +
+    hours +
+    " horas, " +
+    minutes +
+    " minutos e " +
+    seconds +
+    " segundos restantes.";
 }, 1000);
